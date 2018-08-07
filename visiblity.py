3e63c900da9f600b7f9f77b1ec5e6dc686ab188f
CGottardi <37217412+CGottardi@users.noreply.github.com>
2018-08-07 11:18:50 +0200

    Add files via upload

--- visiblity.py
+++ visiblity.py
@@ -1,18 +1,18 @@
 import arcpy
-import numpy
 import os
-import pandas
 import matplotlib.pyplot as plt
 from simpledbf import Dbf5
+
 
 if arcpy.CheckExtension("Spatial") == "Available":
     arcpy.CheckOutExtension('Spatial') #Checkout the extension "Spatial"
 else:
     print "no spatial analyst license available" #Checkout if the extension is working
 
-#*******************************
+
+#*****************************
 #Set environemnt and workspace
-#*******************************
+#*****************************
 myworkspace="D:/Unibern/Geographie/Master/Geodatenanalyse/Projekt/MeineToolbox/ToolData"
 tempdir="D:/temp" #Data that you don't need at the end
 print "Workspace: " + myworkspace
@@ -20,13 +20,13 @@ arcpy.env.workspace= myworkspace #Workspace is set
 arcpy.env.overwriteOutput = True #Allows to overwrite other files (e.g. to overwrite tables)
 
 
-#Settlements shapefile
-settlements=myworkspace + "/" + "Siedlungen" + "/" + "CH_SiedlungenLV03.shp" #Reading in shapefile of settlement points
+#Settlement shapefile
+settlements=myworkspace + "/" + "Siedlungen" + "/" + "CH_SiedlungenLV03.shp" #Reading in the shapefile of settlement points
 
-#dem
-dem=arcpy.Raster(myworkspace + "/" + "dem" + "/" + "swiss200") #Reading in the dem
+#DEM
+dem=arcpy.Raster(myworkspace + "/" + "dem" + "/" + "swiss200") #Reading in the DEM
 arcpy.env.cellSize = dem #Cellsize setting
-arcpy.env.extent = dem #Extent setting/environnment setting)
+arcpy.env.extent = dem #Setting the extent
 arcpy.env.snapRaster = dem #"Tools that honor the Snap Raster environment will adjust the extent of output rasters so that they match the cell alignment of the specified snap raster" (ArcGIs for Desktop 2018).
 #ArcGIS for Desktop (2018). Snap Raster (Environment setting) [online]. Available at: "http://desktop.arcgis.com/en/arcmap/10.3/tools/environments/snap-raster.htm" [last accessed: 01.08.2018].
 
@@ -36,20 +36,20 @@ settl=tempdir+"/"+"settlement.shp"
 
 
 #***********************************************************************************************************************
-#Add Field to the settlement shapefile Use only if these fields do not exist in the attribute table already!
+#Add Field to the settlement shapefile. Use only if these fields do not exist in the attribute table already!
 #***********************************************************************************************************************
 
-#arcpy.AddField_management(settlements, "MaxArea", "FLOAT", "16", "5", "", "", "NULLABLE", "NON_REQUIRED", "") #Adds the field "MaxArea" to the settlement shapefile. Use only if these fields do not exist in the attribute table already!
+#arcpy.AddField_management(settlements, "MaxArea", "FLOAT", "16", "5", "", "", "NULLABLE", "NON_REQUIRED", "") #Adds the field "MaxArea" to the settlement shapefile.
 
 
 #**************************************************************************************************
-#Loop for the visibility function - the area for each settlement is calculated and saved seperatly
+#Loop for the visibility function - the area for each settlement is calculated and saved separately
 #**************************************************************************************************
 
 cursor = arcpy.da.UpdateCursor(settlements, ["FID", "MaxArea"]) #A cursor is made -> same as selecting a row. In the square brackets the fields/columns that you want selected are put in. In this case "FID" the identification number and "MaxArea" where the visible area will be written in.
 for row in cursor: #For each row in this shapefile following commands apply:
     id = row[0] #Row with index 0 is the first row
-    visible_area= os.path.join(myworkspace + "/" + "Results" + "/" + "Viewshed" + "/" "visib" + str (id) + ".tif") #os path join is used, that the visible area of each area is saved separately with its ID number at the end of the name. The str (id) coincides with the real FID of the settlements in the settlement shapefile
+    visible_area= os.path.join(myworkspace + "/" + "Results" + "/" + "Viewshed" + "/" "visib" + str (id) + ".tif") #os path join is used, that the visible area of each settlement is saved separately with its ID number at the end of the name. The str (id) coincides with the real FID of the settlements in the settlement shapefile
     visarea= os.path.join(myworkspace + "/" + "Results" + "/" + "Viewshed" + "/" "visarea" + str (id) + ".tif") #Variable is later used to only extract the count of visible area/cells (the output of the visiblity anlysis gives out an attribute table with two classes: visible and non-visible)
     print "processing FID: " + str(id)
     where_clause = '"FID" = ' + str(id) # This variable will be used for the function arcpy.Select_analysis() to select which settlement is being processed
@@ -69,7 +69,7 @@ for row in cursor: #For each row in this shapefile following commands apply:
 
     #Extract only the visible area to a new raster:
     arcpy.gp.ExtractByAttributes_sa(visible_area, "\"OID\" =1", visarea) #1 stands in this case for the visible area (0 would be non-visible)
-    #Convert attribute tables to dBase and save them. The ID is added to the name as with the rasterfiles before, so that they can be saved separatly:
+    #Convert attribute tables to dBase files and save them. The ID is added to the name as with the raster files before, so that they can be saved separatly:
     arcpy.TableToTable_conversion(visarea, myworkspace + "/" + "Results" + "/" + "Tables", "visarea" + str (id) + ".dbf", "", "Value \"Value\" false true false 10 Long 0 10 ,First,#,D:\\Unibern\\Geographie\\Master\\Geodatenanalyse\\Projekt\\MeineToolbox\\ToolData\\Results\\Viewshed\\visarea + str (id) +.tif\\Band_1,Value,-1,-1;Count \"Count\" false true false 19 Double 0 0 ,First,#,D:\\Unibern\\Geographie\\Master\\Geodatenanalyse\\Projekt\\MeineToolbox\\ToolData\\Results\\Viewshed\\visarea + str (id) +.tif\\Band_1,Count,-1,-1;Area \"Area\" true true false 10 Long 0 10 ,First,#,D:\\Unibern\\Geographie\\Master\\Geodatenanalyse\\Projekt\\MeineToolbox\\ToolData\\Results\\Viewshed\\visarea + str (id) +.tif\\Band_1,Area,-1,-1;ID \"ID\" true true false 5 Short 0 5 ,First,#,D:\\Unibern\\Geographie\\Master\\Geodatenanalyse\\Projekt\\MeineToolbox\\ToolData\\Results\\Viewshed\\visarea + str (id) +.tif\\Band_1,ID,-1,-1", "")
     cursor.updateRow(row) #The cursor goes to the next row/next settlement
 del row #You don't have to do that, but if you create another cursor it is sometimes better to delete the old one
@@ -91,11 +91,11 @@ arcpy.JoinField_management(myworkspace2 +"/" + "visiblareajoin.dbf", "ID", settl
 #Visualisation in a bar plot
 #*****************************
 
-visblareadbf= Dbf5(myworkspace2 + "/" + "visiblareajoin.dbf") #Convert dbf-File into a data frame
-dfvisarea= visblareadbf.to_dataframe() #Convert dBase-File into a data frame
+visblareadbf= Dbf5(myworkspace2 + "/" + "visiblareajoin.dbf") #Convert dBase file into a data frame
+dfvisarea= visblareadbf.to_dataframe() #Convert dBase file into a data frame
 plt.hist(dfvisarea["Area"],bins=56) #Command for a histogramm and how many bars you want to have maximally displayed. In this case 56 were chosen because there are 56 settlements.
-plt.xlabel("m2") #Here you set your label for the x-axis
-plt.ylabel ("number of villages") #Here you set your label for the y-axis
+plt.xlabel("m2") #Here you set the title of the x-axis
+plt.ylabel ("number of villages") #Here you set the title of the y-axis
 plt.title("Visible area per village") #Title of the plot
 plt.show() #Pop-up of the figure
 plt.savefig(myworkspace + "/" +"Results" + "/" + "histovisarea.png") #Saving the plot as a png
@@ -104,12 +104,12 @@ plt.savefig(myworkspace + "/" +"Results" + "/" + "histovisarea.png") #Saving the
 #Calculate the mean and the standard deviation of the visible area
 #*******************************************************************
 
-arcpy.Statistics_analysis(myworkspace2 +"/" + "visiblareajoin.dbf", myworkspace +"/" + "Results" + "/" + "StatisticsVIS.dbf", "Area MEAN;Area STD", "") #Function for calculation of the mean and the standard deviation
+arcpy.Statistics_analysis(myworkspace2 +"/" + "visiblareajoin.dbf", myworkspace +"/" + "Results" + "/" + "StatisticsVIS.dbf", "Area MEAN;Area STD", "") #Function for the calculation of the mean and the standard deviation
 
-statisticsVISdbf= Dbf5(myworkspace + "/" + "Results" + "/" "StatisticsVIS.dbf") #Convert dBase-File into a data frame
-statisticsVIS= statisticsVISdbf.to_dataframe() #Convert dBase-File into a data frame
-statisticsVISmean= statisticsVIS["MEAN_Area"] #Setting variable of the mean
-statisticsVISstd= statisticsVIS["STD_Area"] #Setting variable of the standard deviation
+statisticsVISdbf= Dbf5(myworkspace + "/" + "Results" + "/" "StatisticsVIS.dbf") #Convert dBase file into a data frame
+statisticsVIS= statisticsVISdbf.to_dataframe() #Convert dBase file into a data frame
+statisticsVISmean= statisticsVIS["MEAN_Area"] #Setting the variable of the mean
+statisticsVISstd= statisticsVIS["STD_Area"] #Setting the variable of the standard deviation
 
 COV=statisticsVISstd*100/statisticsVISmean #Calculating the coefficient of variance
 print COV #The coefficient of variance is displayed in the console
